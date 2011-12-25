7f71b779e3cab995826ed57e6afb5e4d8f7e563d
Roman Valiouline <roman.valiouline@gmail.com>
2011-12-24 21:32:16 -0500

    Finish 1/2 of user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
-  #get "pages/home"
-  #get "pages/contact"
-  #get "pages/about"
-  #get "pages/help"
+  resources :users
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
